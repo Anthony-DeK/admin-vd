13c932e5c03d46d9fe5f97279ddc64b54c6cdb9a
Anthony-DeK <adekorwin@gmail.com>
2026-03-30 19:06:36 +0200

    Delete src directory

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { BookingList } from './BookingList';
-export { BookingForm } from './BookingForm';
-export { BookingCalendar } from './BookingCalendar';
-export { ApartmentList } from './ApartmentList'; 
